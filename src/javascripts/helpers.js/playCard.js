4874f1bbc3ba66eb6f9c193f920538243e3a33fe
Ben Jarrett <b.cl.jarrett@gmail.com>
2021-02-28 22:34:25 -0600

    Changed font to discgusting Tamagotchi like font

--- src/javascripts/helpers.js/playCard.js
+++ src/javascripts/helpers.js/playCard.js
@@ -6,7 +6,7 @@ const fun = 50;
 const playValue = () => {
   let playCard = '';
   playCard += `<h1>PLAY</h1>
-  <div id="play-fun" class="text-center">${fun}</div>
+  <div id="play-fun" class="text-center"> <h2>${fun}</h2></div>
   <br/><button type="button" id="addSuperFun" class="btn btn-light btn btn-outline-primary"> <div class="fa fa-gamepad"</div> Super Fun </button>
   <button type="button" id="addSlightlyFun" class="btn btn-light btn btn-outline-secondary"> <div class="fa fa-puzzle-piece" </div> Slightly Fun</button>
   `;
